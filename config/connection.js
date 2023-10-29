ca1ec5301d03a0a03fbb2d719c2fcd2c99491735
asoto225 <135178400+abel25xD@users.noreply.github.com>
2023-10-29 15:58:40 -0700

    models created and updated, npm init created with dependecies

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const Sequlize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequlize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequlize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD, 
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
+
+module.exports = sequelize;
